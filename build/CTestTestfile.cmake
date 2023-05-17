0b5ac60a45c397e88aed86583e3e682d66bfe670
mistermiki <mistermiki28@gmail.com>
2023-05-17 13:42:39 +0200

    Initial Commit. First example with references.

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/w85wos/Documents/Cpp samples/cpp-samples
+# Build directory: C:/Users/w85wos/Documents/Cpp samples/cpp-samples/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
